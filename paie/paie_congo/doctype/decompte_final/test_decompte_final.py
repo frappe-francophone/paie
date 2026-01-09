a2dbc2b5904ba5be92de8dbe489b31e267e6e462
frappe-francophone <frappefrancophone@email.com>
2026-01-09 15:53:26 +0100

    Decompte Ajouté

---
+++ paie/paie_congo/doctype/decompte_final/test_decompte_final.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Richard and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDecompteFinal(FrappeTestCase):
+	pass
